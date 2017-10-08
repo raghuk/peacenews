25de7d5078e35569419903bf3bdb7d3416fab5e3
Raghu Kumar <raghu.kumar@cognistreamer.com>
2017-10-08 20:53:47 +0530

    missed, now added vector icons

--- android/app/src/main/java/com/gws/peacenews/MainApplication.java
+++ android/app/src/main/java/com/gws/peacenews/MainApplication.java
@@ -3,6 +3,7 @@ package com.gws.peacenews;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new ReactNativeOneSignalPackage(),
             new SplashScreenReactPackage()
